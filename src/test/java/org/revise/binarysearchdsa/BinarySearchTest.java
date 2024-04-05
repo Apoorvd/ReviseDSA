6d2d52a900bf135a656d442cb80b9fce75bb3ff4
jojo <d123apoorv@gmail.com>
2024-04-05 14:46:04 +0530

    commit BinarySearch

---
+++ src/test/java/org/revise/binarysearchdsa/BinarySearchTest.java
@@ -0,0 +1,31 @@
+package org.revise.binarysearchdsa;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinarySearchTest {
+
+    @Test
+    void binarySearchTest()
+    {
+        var search = new BinarySearch();
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        //List<Integer> list2 = new ArrayList<>(){{add(3);}};
+        //List<String> list = Arrays.asList("foo", "bar");
+        //List<String> list = Stream.of("foo", "bar")
+        //      .collect(Collectors.toList());
+        //List<String> list = List.of("foo", "bar", "baz");
+        //Set<String> set = Set.of("foo", "bar", "baz");
+
+        int testResult = search.binarySearch(list, 3);
+        assertEquals(3, testResult+1);
+
+        testResult = search.binarySearch(list, 11);
+        assertEquals(-1, testResult);
+    }
+}
